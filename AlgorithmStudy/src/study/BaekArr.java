b0fcc1ac08357a77db4615c787b6afcecf507be6
jinju <jinju@DESKTOP-M4KM66O>
2021-04-06 17:03:41 +0900

    3052번 커밋><!!

--- AlgorithmStudy/src/study/BaekArr.java
+++ AlgorithmStudy/src/study/BaekArr.java
@@ -56,4 +56,39 @@ class BaekArr {
 		
 	}
 	
+	@Test
+	void algo3052() {
+		//나머지
+		Scanner sc = new Scanner(System.in);
+		
+		int arr[]  = new int[10]; //입력된 숫자
+		
+		for(int i=0; i<arr.length; i++){
+			
+			arr[i] = sc.nextInt() % 42;
+			
+		}
+		sc.close();
+		
+		int cnt = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			int a = 0;
+			
+			for (int j = i+1; j < arr.length; j++) {
+				
+				if(arr[i] == arr[j]) {
+					
+					a++;
+				}
+			}
+			
+			if( a == 0) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+	
 }
