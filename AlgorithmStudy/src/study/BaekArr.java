3753ddf749bcaa2a35095077fc6ee4c278d1e207
jinju <jinju@DESKTOP-M4KM66O>
2021-04-06 13:22:06 +0900

    algo2562 commit;

--- AlgorithmStudy/src/study/BaekArr.java
+++ AlgorithmStudy/src/study/BaekArr.java
@@ -19,12 +19,41 @@ class BaekArr {
 		for (int i = 0; i <N; i++) {
 			arr[i] = sc.nextInt();
 		}
-		
+	
 		Arrays.sort(arr);
 		sc.close();
 		
 		System.out.println(arr[0] + " " + arr[N-1]);
 		
 	}
+	
+	@Test
+	void algo2562() {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int arr[] = new int[9];
+		
+		for(int i = 0; i < arr.length; i++) {
+			
+			arr[i] = sc.nextInt();
+		}
+		
+		int max = arr[0];
+		int a = 1;
 
+		for (int j = 0; j < arr.length; j++) {
+		
+			if(max < arr[j]) {
+				max = arr[j];
+				a = j+1;
+			}
+		}
+		sc.close();
+		
+		System.out.println(max);
+		System.out.println(a);
+		
+	}
+	
 }
